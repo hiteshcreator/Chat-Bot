8a7ef3b0fd9e440e9ea0598e0f975c29b7a51b2b
Hitesh Sharma <thehiteshcreator@gmail.com>
2023-06-03 21:02:28 +0530

    Add Question

--- src/component/chatbot.js
+++ src/component/chatbot.js
@@ -1,20 +1,86 @@
 import React from 'react'
 import ChatBot from 'react-simple-chatbot';
 import {ThemeProvider } from 'styled-components'
+import Review from './review';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 const chatbot = () => {
-    const steps = [
-        {
-        id: 'hello-world',
-        message: 'Hello World!',
-        end: true,
-        },
-    ];
+  const refresh = () => window.location.reload(true)
+
+  const steps = [
+    {
+      id: '1',
+      message: 'What is your name?',
+      trigger: 'name',
+    },
+    {
+      id: 'name',
+      user: true,
+      trigger: '3',
+    },
+    {
+      id: '3',
+      message: 'Hi {previousValue}! What is your gender?',
+      trigger: 'gender',
+    },
+    {
+      id: 'gender',
+      options: [
+        { value: 'male', label: 'Male', trigger: '5' },
+        { value: 'female', label: 'Female', trigger: '5' },
+      ],
+    },
+    {
+      id: '5',
+      message: 'How old are you?',
+      trigger: 'age',
+    },
+    {
+      id: 'age',
+      user: true,
+      trigger: '7',
+      validator: (value) => {
+        if (isNaN(value)) {
+          return 'value must be a number';
+        } else if (value < 0) {
+          return 'value must be positive';
+        } else if (value > 120) {
+          return `${value}? Come on!`;
+        }
+
+        return true;
+      },
+    },
+    {
+      id: '7',
+      message: 'Great! Check out your summary',
+      trigger: 'review',
+    },
+    {
+      id:'review',
+      component: <Review />,
+      asMessage: true,
+      trigger: 'next',
+    },
+    {
+      id:"next",
+      message:"Thanks you for using chat-bot!",
+      trigger:"refresh"
+    },
+    {
+      id: 'refresh',
+      options: [
+        { value: 'male', label: <><RefreshIcon  onClick={refresh} /></>},
+      ],
+      end:true
+    }
+
+  ];
 
   return (
 
     <ThemeProvider theme={theme}>
-      <ChatBot steps={steps} />
+      <ChatBot steps={steps} headerTitle="Chat Bot" />
     </ThemeProvider>
 
   );
---
+++ src/component/review.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const review = ({steps}) => {
+  const {name,age,gender} = steps;
+
+  return (
+    <div style={{ width: '100%' }}>
+        <h3>Summary</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>{name.value}</td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>{gender.value}</td>
+            </tr>
+            <tr>
+              <td>Age</td>
+              <td>{age.value}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+  );
+}
+
+export default review
